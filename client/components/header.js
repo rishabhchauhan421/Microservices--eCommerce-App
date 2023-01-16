bf39dcc0166f411c9928e9668762e0818918f1af
Rishabh <rshbhChauhan@gmail.com>
2023-01-16 21:43:36 +0530

    Server side rendering using next.js Added

---
+++ client/components/header.js
@@ -0,0 +1,27 @@
+const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <a href={href} className="nav-link">{label}</a>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a href="/" className="navbar-brand">LOGO</a>
+
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
